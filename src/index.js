0bdecd1d9d0225c94065274c786264f8dfacfa38
Cesar Mejias <cesarfar376@gmail.com>
2022-03-31 23:06:54 -0400

    primer commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const messages = [
+  "David",
+  "Cesar",
+  "Gabriel",
+  "Diana",
+  "Ana Maria",
+  "Isabela",
+  "Antonio",
+  "Norma"
+]
+
+const randomMsg = () => {
+  const message = messages[Math.floor(Math.random() * messages.length)]
+  console.log(message)
+}
+
+module.exports = { randomMsg }
